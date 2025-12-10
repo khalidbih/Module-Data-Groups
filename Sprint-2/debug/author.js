8de5a0d210b2d28484008b44909030e072ab9533
khalidbih <kain19m@hotmail.com>
2025-12-10 22:57:39 +0000

    Fixed for-- log object values using Object.values

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -2,6 +2,8 @@
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+//The for of loop works on things you can loop over, like arrays or strings.
+//A plain object (author) isn’t something you can loop over directly, so for (const value of author) doesn’t work.
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +13,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
+
   console.log(value);
 }
