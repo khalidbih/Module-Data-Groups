22082c4e0f2c3d704693e9a3fb622653d9ea0bb4
khalidbih <kain19m@hotmail.com>
2025-12-10 23:14:04 +0000

    Fix recipe logging with ingredients on separate lines

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,12 +4,14 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+// Printing the ingredients array directly won’t show each item on a new line.
+// Use join("\n") to display each ingredient on its own line.
+
+
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves}\ningredients:\n${recipe.ingredients.join("\n")}`);
